635f14c33284e2a37f8d9c39eea08d1bbe803b84
supun <supun.gamage@ebuilder.com>
2017-02-26 00:04:51 +0530

    added Timer test

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,11 +1,60 @@
 var React = require('react');
+var Clock = require('Clock');
+var Controls = require('Controls');
 
-var Timer = () => {
-  return(
-    <div>
-      Timmer.jsx
-    </div>
-  );
-}
+var Timer = React.createClass({
+
+  getInitialState: function() {
+    return {
+      count: 0,
+      timerStatus: 'stopped'
+    };
+  },
+  handleStatusChange: function(newStatus) {
+    this.setState({
+      timerStatus: newStatus
+    });
+  },
+  componentDidUpdate: function(prevProps, prevState) {
+    if(this.state.timerStatus !== prevState.timerStatus) {
+      switch (this.state.timerStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+        case 'stopped':
+          this.setState({
+            count: 0
+          });
+        case 'paused':
+          clearInterval(this.timer);
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+  startTimer: function () {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count + 1;
+      this.setState({
+        count: newCount
+      });
+    }, 1000);
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
+  render: function() {
+    var {count, timerStatus} = this.state;
+
+    return(
+      <div>
+        <h1 className="page-title">Timer App</h1>
+        <Clock totalSeconds={count}/>
+        <Controls countdownStatus={timerStatus} onStatusChange={this.handleStatusChange}/>
+      </div>
+    );
+  }
+});
 
 module.exports = Timer;
